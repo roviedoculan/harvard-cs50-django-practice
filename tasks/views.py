b7df30328c9eee70496d3d4f8f52ef8ed309962c
Rovie Doculan <rovie.doculan@gmail.com>
2021-03-27 18:29:18 +0800

    added display logic for tasks

--- tasks/views.py
+++ tasks/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 tasks = ["foo", "bar", "baz"]
 
 # Create your views here.
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
